e4bfde67457db96365959b00f48f5d170bbf661d
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-03-26 20:23:24 +0100

    feat: used gob to encode struct rather then string

--- main.go
+++ main.go
@@ -2,35 +2,47 @@ package main
 
 import (
 	"bufio"
+	"encoding/gob"
 	"flag"
-	"fmt"
 	"net"
 	"os"
 
 	log "github.com/charmbracelet/log"
 )
 
+type Messsage struct {
+	Username string
+	Message  string
+}
+
 func main() {
+	gob.Register(Messsage{})
+	log.SetLevel(log.DebugLevel)
+	var username string
 	var dialAddress string
 	flag.StringVar(&dialAddress, "address", "", "Address to dial")
+	flag.StringVar(&username, "username", "", "Username to use")
 	flag.Parse()
 	if dialAddress != "" {
-		log.Info("Dialing to address: ", "addr", dialAddress)
+		log.Debug("Dialing to address: ", "addr", dialAddress)
 	}
 	if dialAddress == "" {
 		Server()
 	} else {
-		Client(dialAddress)
+		Client(username, dialAddress)
 	}
 }
 
 func Server() {
-	log.Info("Starting server")
+	log.Debug("Starting server")
+	// print the server address
+	var ip = getOutboundIP()
+	log.Debug("Server address:", "addr", ip.String())
 	// Listen on TCP port 8080 on all available unicast and
 	// anycast IP addresses of the local system.
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println(err)
+		log.Error("Error listening:", "err", err)
 		return
 	}
 	defer listener.Close()
@@ -46,9 +58,8 @@ func Server() {
 	}
 }
 
-func Client(dialAddress string) {
-	log.Info("Starting client")
-
+func Client(username string, dialAddress string) {
+	log.Debug("Starting client")
 	// Connect to the server
 	// Dial the server at the address provided
 	conn, err := net.Dial("tcp", dialAddress)
@@ -57,37 +68,62 @@ func Client(dialAddress string) {
 		return
 	}
 	defer conn.Close()
-
 	// read stdin
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Message: ")
-	text, _ := reader.ReadString('\n')
-	log.Info("Message sent:", "message", text)
 
-	// Create a new writer for the connection
-	// Write a message to the server
-	writer := bufio.NewWriter(conn)
-	_, err = writer.WriteString(text)
-	if err != nil {
-		log.Error("Error writing message:", "err", err)
-		return
+	for {
+		log.Info("Enter message to send:")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Error("Error reading input:", "err", err)
+			break
+		}
+		msg := Messsage{Username: username, Message: text}
+		sendMessage(conn, msg)
+		log.Info("Message sent:", "message", text)
 	}
-	writer.Flush()
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close() // Close the connection when the function returns
-
-	// Create a new reader for the connection
-	reader := bufio.NewReader(conn)
-
 	for {
-		// Read a message from the client
-		message, err := reader.ReadString('\n')
+		msg, err := readMessage(conn)
 		if err != nil {
-			log.Error("Error reading message:", "err", err)
-			return
+			break
 		}
-		log.Info("Message received:", "message", message)
+
+		log.Info("Message received:", "username", msg.Username, "message", msg.Message)
 	}
+}
+
+// Get preferred outbound ip of this machine
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
+func sendMessage(conn net.Conn, msg Messsage) {
+	enc := gob.NewEncoder(conn)
+	err := enc.Encode(msg)
+	if err != nil {
+		log.Error("Error sending message to server:", "err", err)
+	}
+}
+
+func readMessage(conn net.Conn) (Messsage, error) {
+	dec := gob.NewDecoder(conn)
+	var msg Messsage
+	err := dec.Decode(&msg)
+	if err != nil {
+		log.Error("Error reading message from server:", "err", err)
+		return msg, err
+	}
+	return msg, nil
 }
